a22103364bb9048c50b5b4e163245b87346b6c79
lidastonebee <am.papoutsa@gmail.com>
2024-10-14 21:38:39 +0300

    Update check.c

--- check.c
+++ check.c
@@ -1,4 +1,4 @@
 # Check out solution URL
 $ cat solution.txt
 # Ensure the URL exists
-$ curl --output /dev/null --silent --head --fail YourUsername.github.io && \echo "URL exists" || echo "URL does not exist"
+$ curl --output /dev/null --silent --head --fail lidastonebee.github.io && \echo "URL exists" || echo "URL does not exist"
